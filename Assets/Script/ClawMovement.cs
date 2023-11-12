ef6965e331f561516b8480939b1f1c88fac7fe94
kimikokel <kellyleong2002@gmail.com>
2023-11-12 21:10:42 +0800

    claw move final, instantiate (minor bug)

--- Assets/Script/ClawMovement.cs
+++ Assets/Script/ClawMovement.cs
@@ -12,79 +12,60 @@ public class ClawMovements : MonoBehaviour {
 
     public Transform HookHeight;
 
-    // These are simple cubes used to define movement limits
     public Transform leftLimit;
     public Transform rightLimit;
     public Transform frontLimit;
     public Transform backLimit;
     public bool CanControl;
     bool onSpace;
+    bool isEmpty;
 
     public GameObject[] balls;
     public Transform clawPosition;
+    GameObject newBall;
 
     void Awake(){
-        Instantiate(balls[Random.Range(0,balls.Length)], clawPosition.position, clawPosition.rotation);
     }
 
-    // Use this for initialization
     void Start () {
         rigidBody = GetComponent<Rigidbody>();
         CanControl = true;
         onSpace = false;
+        isEmpty = true;
         // Instantiate(balls[Random.Range(0,balls.Length)], clawPosition.position, clawPosition.rotation);
-        // clawAnimator.Play("close");
     }
 
-    // Update is called once per frame
     void Update() {
         if (CanControl) {
-            // Lateral movements
+
             if (transform.position.x > leftLimit.transform.position.x) {
-                if (Input.GetKey(KeyCode.A)) {
-                    // transform.Translate(clawSpeed * -1 * Time.deltaTime, 0, 0);
-                    // transform.position = new Vector3((clawSpeed * -5 * Time.deltaTime),transform.position.y,transform.position.z);
+                if (Input.GetKey(KeyCode.D)) {
+                    transform.Translate(clawSpeed * -1 * Time.deltaTime, 0, 0);
                 }
             }
 
             if (transform.position.x < rightLimit.transform.position.x) {
-                if (Input.GetKey(KeyCode.D)) {
-                    // transform.Translate(clawSpeed * Time.deltaTime, 0, 0);
-                    // transform.position = new Vector3((clawSpeed * Time.deltaTime),transform.position.y,transform.position.z);    
+                if (Input.GetKey(KeyCode.A)) {
+                    transform.Translate(clawSpeed * Time.deltaTime, 0, 0);
                 }
             }
 
-            // Forward movements
             if (transform.position.z < backLimit.transform.position.z) {
-                if (Input.GetKey(KeyCode.W)) {
-                    // transform.Translate(0, 0, clawSpeed * Time.deltaTime);
-                    // transform.position = new Vector3(transform.position.x,transform.position.y,(clawSpeed * Time.deltaTime)); 
+                if (Input.GetKey(KeyCode.S)) {
+                    transform.Translate(0, 0, clawSpeed * Time.deltaTime);
                 }
             }
 
             if (transform.position.z > frontLimit.transform.position.z) {
-                if (Input.GetKey(KeyCode.S)) {
-                    // transform.Translate(0, 0, clawSpeed * -1 * Time.deltaTime);
-                    // transform.position = new Vector3(transform.position.x,transform.position.y,(clawSpeed * -1 * Time.deltaTime)); 
+                if (Input.GetKey(KeyCode.W)) {
+                    transform.Translate(0, 0, clawSpeed * -1 * Time.deltaTime);
                 }
             }
 
-            // if (Input.GetKey(KeyCode.Space)) {
-            //     // StartCoroutine(returnHook());
-            //     if (HookHeight.position.y > minHeight) {
-            //         transform.Translate(0, clawSpeed * -1 * Time.deltaTime, 0);
-            //         CanControl = false;
-            //     }
-            // }
-                
-            // if (HookHeight.position.y <= maxHeight && !CanControl) {
-            //     transform.Translate(0, clawSpeed * Time.deltaTime, 0);
-            //     // CanControl = true;
-            // }
-
-            if (Input.GetKey(KeyCode.Space)) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
                 onSpace = true;
                 CanControl = false;
+                // isEmpty = true;
                 print("space "+ (int)HookHeight.position.y);
             }
         }
@@ -92,279 +73,37 @@ public class ClawMovements : MonoBehaviour {
             if ((int)HookHeight.position.y >= minHeight && onSpace) {
                 transform.Translate(0, (clawSpeed * -1f * Time.deltaTime), 0);
                 print("down "+ (int)HookHeight.position.y);
+                clawAnimator.Play("open");
             }         
 
-            ///////////////////////
 
             if ((int)HookHeight.position.y == minHeight) {
-                // GameObject.FindWithTag("ClawBar").transform.Rotate(0.0f, 0.0f, 20.0f);
                 print("==max");
                 onSpace = false;
-                clawAnimator.Play("open");
-                StartCoroutine(sleep(2f));
+                StartCoroutine(closeClaw(0.2f));
+                isEmpty = true;
             }
-                
-            // if (HookHeight.position.y <= maxHeight && !onSpace && HookHeight.position.y > minHeight) {
-            //     // GameObject.FindWithTag("ClawBar").transform.Rotate(0.0f, 0.0f, 20.0f);
-            //     StartCoroutine(sleep(0.5f));
-            //     // print("up");
-            //     // transform.Translate(0, (clawSpeed * Time.deltaTime), 0);
-            //     CanControl = true;
-            //     // clawAnimator.Play("normal");
-            // }
 
-            IEnumerator sleep(float secs) {
+            if ((int)HookHeight.position.y == maxHeight && isEmpty) {
+                // StartCoroutine(spawn(0.2f));
+                isEmpty = false;
+                newBall = Instantiate(balls[Random.Range(0,balls.Length)], clawPosition.position, clawPosition.rotation);
+            }
+
+            IEnumerator closeClaw(float secs) {
                 yield return new WaitForSeconds(secs); 
                 if (HookHeight.position.y <= maxHeight && !onSpace && HookHeight.position.y > minHeight) {
-                    // GameObject.FindWithTag("ClawBar").transform.Rotate(0.0f, 0.0f, 20.0f);
-                    // StartCoroutine(sleep(0.5f));
                     print("up");
                     transform.Translate(0, (clawSpeed * Time.deltaTime), 0);
                     CanControl = true;
-                    // yield return new WaitForSeconds(secs); 
                     clawAnimator.Play("close");
-                    // Instantiate(balls[Random.Range(0,balls.Length)], clawPosition.position, clawPosition.rotation);
                 }
             }
+
+            IEnumerator spawn(float secs) {
+                yield return new WaitForSeconds(secs);
+                isEmpty = false;
+                newBall = Instantiate(balls[Random.Range(0,balls.Length)], clawPosition.position, clawPosition.rotation);
+            }
     }
-        
-
-        //     Lower the hook
-        //     if (HookHeight.position.y > minHeight) {
-        //         if (Input.GetKey(KeyCode.Space)) {
-        //             transform.Translate(0, clawSpeed * -1 * Time.deltaTime, 0);
-        //             // print("rotate");
-        //             // GameObject.FindWithTag("ClawBar").transform.Rotate(0.0f, 0.0f, -50.0f);
-        //         }
-        //     } else {
-        //         CanControl = false;
-        //     }
-        // }
-
-    
-
-    // IEnumerator returnHook() {
-    //     yield return new WaitForSeconds(0.2f);  
-    //     print("==max");
-    //     onSpace = false;
-    //     clawAnimator.Play("close");
-    //     // yield return new WaitForSeconds(0.2f);    
-
-    //     if (HookHeight.position.y <= maxHeight && !onSpace && HookHeight.position.y > minHeight) {
-    //         // GameObject.FindWithTag("ClawBar").transform.Rotate(0.0f, 0.0f, 20.0f);
-    //         print("up");
-    //         transform.Translate(0, (clawSpeed * Time.deltaTime), 0);
-    //         CanControl = true;
-    //         // clawAnimator.Play("normal");
-    //     }
-    // }
-
-}
-//     void FixedUpdate () {
-//         // Attach the motor's X/Z movement to the hook
-//         Motor.transform.position = new Vector3(transform.position.x, Motor.transform.position.y, transform.position.z);
-//         Tubes.transform.position = new Vector3(Tubes.transform.position.x, Tubes.transform.position.y, Motor.transform.position.z + tubeSeparationCorrection);
-
-//         // Release the prize
-//         if (ReleasePrize) {
-//             if (HookHeight.position.y <= maxHeight) {
-//                 transform.Translate(0, clawSpeed * Time.deltaTime, 0);
-//             }
-
-//             if (true) {
-//                 // Debug.Log("Inside the basket");
-//                 insideBasket = true;
-
-//                 // Start a coroutine to lower the hook and release the prize
-//                 if (insideBasket) {
-//                     StartCoroutine(ReleasePrizeInBasket(1.5f));
-//                     insideBasket = false;
-//                 }
-//             }
-//         }
-
-//         if (lowerHookAndReleasePrize) {
-//             if (HookHeight.position.y > minHeight) {
-//                 // Debug.Log("Lowering");
-//                 // transform.Translate(0, clawSpeed * -1 * Time.deltaTime, 0);
-//                 StartCoroutine(OpenClawInBasket(0.5f));
-//                 lowerHookAndReleasePrize = false;
-//             } else {
-//                 StartCoroutine(OpenClawInBasket(0.5f));
-//                 lowerHookAndReleasePrize = false;
-//             }
-//         }
-
-//         if (raiseHookOutOfBasket) {
-//             if (HookHeight.position.y <= maxHeight) {
-//                 transform.Translate(0, clawSpeed * Time.deltaTime, 0);
-//             } else {
-//                 CanControl = true;
-//                 raiseHookOutOfBasket = false;
-//             }
-//         }
-
-//         if (CanControl) {
-//             // Lateral movements
-//             if (transform.position.x > leftLimit.transform.position.x) {
-//                 if (Input.GetKey(KeyCode.A)) {
-//                     transform.Translate(speed * -1 * Time.deltaTime, 0, 0);
-//                 }
-//             }
-
-//             if (transform.position.x < rightLimit.transform.position.x) {
-//                 if (Input.GetKey(KeyCode.D)) {
-//                     transform.Translate(speed * Time.deltaTime, 0, 0);
-//                 }
-//             }
-
-//             // Forward movements
-//             if (transform.position.z < backLimit.transform.position.z) {
-//                 if (Input.GetKey(KeyCode.W)) {
-//                     transform.Translate(0, 0, speed * Time.deltaTime);
-//                 }
-//             }
-
-//             if (transform.position.z > frontLimit.transform.position.z) {
-//                 if (Input.GetKey(KeyCode.S)) {
-//                     transform.Translate(0, 0, speed * -1 * Time.deltaTime);
-//                 }
-//             }
-
-//             // Lower the hook
-//             if (HookHeight.position.y > minHeight) {
-//                 if (Input.GetKey(KeyCode.Space)) {
-//                     transform.Translate(0, hookSpeed * -1 * Time.deltaTime, 0);
-//                     // print("rotate");
-//                     // GameObject.FindWithTag("ClawBar").transform.Rotate(0.0f, 0.0f, -50.0f);
-//                 }
-//             } else {
-//                 StartCoroutine(CloseClaw(0.5f));
-//                 CanControl = false;
-//             }
-//         }
-//     }
-
-//     // Coroutine to collect the prize
-//     IEnumerator CloseClaw(float waitTime) {
-//         yield return new WaitForSeconds(waitTime);
-//         CloseClaw();
-//         yield return new WaitForSeconds(waitTime);
-//         // Debug.Log("Raising hook");
-//         ReleasePrize = true;
-//     }
-
-//     // Coroutine to release the prize
-//     IEnumerator ReleasePrizeInBasket(float waitTime) {
-//         yield return new WaitForSeconds(waitTime);
-//         lowerHookAndReleasePrize = true;
-//         ReleasePrize = false;
-//         // Debug.Log("Lowering hook");
-//         yield return new WaitForSeconds(waitTime);
-//     }
-
-//     // Coroutine to release the prize
-//     IEnumerator OpenClawInBasket(float waitTime) {
-//         yield return new WaitForSeconds(waitTime);
-//         OpenClaw();
-//         yield return new WaitForSeconds(waitTime);
-//         raiseHookOutOfBasket = true;
-//     }
-
-//     public void OpenClaw() {
-//         // Debug.Log("Opening Hook");
-//         // clawAnimator.SetBool("Open", true);
-//         // clawAnimator.SetBool("Close", false);
-//     }
-
-//     public void CloseClaw() {
-//         Debug.Log("Closing Hook");
-//         // clawAnimator.SetBool("Open", false);
-//         // clawAnimator.SetBool("Close", true);
-//         // clawAnimator.Play("Close");
-//     }
-// }
-
-// using UnityEngine;
-// using System.Collections;
-
-// public class ClawMovements : MonoBehaviour {
-
-//     public float maxHeight;
-//     public float minHeight;
-//     public float tubeSeparationCorrection;
-
-//     public GameObject Tubes;
-//     public GameObject Motor;
-//     public GameObject Hook;
-//     public Animator clawAnimator;
-
-//     public float speed;
-//     public float clawSpeed;
-//     Rigidbody rigidBody;
-
-//     public Transform HookHeight;
-
-//     public Transform leftLimit;
-//     public Transform rightLimit;
-//     public Transform frontLimit;
-//     public Transform backLimit;
-
-//     public bool CanControl;
-//     bool ReleasePrize;
-
-//     // Use this for initialization
-//     void Start () {
-//         clawAnimator = Hook.gameObject.GetComponent<Animator>();
-//         rigidBody = GetComponent<Rigidbody>();
-//         CanControl = true;
-//         ReleasePrize = false;
-//     }
-
-//     // Update is called once per frame
-//     void Update() {
-//         // Handle player controls for movement
-//         if (CanControl) {
-//             // Movements
-//             float horizontalInput = Input.GetAxis("Horizontal");
-//             float verticalInput = Input.GetAxis("Vertical");
-
-//             // Move the claw horizontally and forward/backward
-//             Vector3 movement = new Vector3(horizontalInput * speed * Time.deltaTime, 0, verticalInput * speed * Time.deltaTime);
-//             transform.Translate(movement);
-
-//             // Lower the Claw
-//             if (Input.GetKey(KeyCode.Space) && HookHeight.position.y > minHeight) {
-//                 transform.Translate(0, clawSpeed * -1 * Time.deltaTime, 0);
-//             } else if (ReleasePrize) {
-//                 // Raise the claw after releasing the prize
-//                 if (HookHeight.position.y < maxHeight) {
-//                     transform.Translate(0, clawSpeed * Time.deltaTime, 0);
-//                 } else {
-//                     ReleasePrize = false; // Reset the flag
-//                 }
-//             }
-//         }
-//     }
-
-//     // Coroutine to collect the prize
-//     IEnumerator CloseClaw(float waitTime) {
-//         yield return new WaitForSeconds(waitTime);
-//         CloseClaw();
-//         yield return new WaitForSeconds(waitTime);
-//         Debug.Log("Raising hook");
-//         ReleasePrize = true;
-//     }
-
-//     public void OpenClaw() {
-//         Debug.Log("Opening Hook");
-//         // clawAnimator.SetBool("Open", true);
-//         // clawAnimator.SetBool("Close", false);
-//     }
-
-//     public void CloseClaw() {
-//         Debug.Log("Closing Hook");
-//         // clawAnimator.SetBool("Open", false);
-//         // clawAnimator.SetBool("Close", true);
-//     }
-// }
+}     
---
+++ Assets/Script/Gravity.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    Vector3 customGravity; // Set your custom gravity here
+
+    void Start()
+    {
+        customGravity = new Vector3(0,-500,0);
+
+        if (rigidBody == null)
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+
+        // Set the custom gravity for this specific rigidbody
+        rigidBody.useGravity = false; // Disable default gravity
+    }
+
+    void FixedUpdate()
+    {
+        // Apply custom gravity
+        rigidBody.AddForce(customGravity, ForceMode.Acceleration);
+    }
+}
